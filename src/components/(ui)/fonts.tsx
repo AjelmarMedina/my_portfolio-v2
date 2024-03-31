17772d7d461e96471a24e0c29a8270e0d5ac4842
Ajelmar Medina <ajelmarmedina@gmail.com>
2024-04-01 00:21:17 +0800

    Fonts

---
+++ src/components/(ui)/fonts.tsx
@@ -0,0 +1,7 @@
+import { Inter, Zilla_Slab } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const zillaSlab = Zilla_Slab({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"]
+})
