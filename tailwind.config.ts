b5f8afc7c61ce790830c640f47b8cb308b9dc8d2
Ajelmar Medina <ajelmarmedina@gmail.com>
2024-04-01 20:14:04 +0800

    shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,6 +39,13 @@ const config: Config = {
         900: '#001833',
       }
     },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -112,13 +119,28 @@ const config: Config = {
             'line-height': '18px'
           }
         },
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
+    require("tailwindcss-animate"),
   ],
 };
 export default config;
