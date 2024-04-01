36eecc9015d25254fbacdbb2ae8c2d9ee44572bd
Ajelmar Medina <ajelmarmedina@gmail.com>
2024-04-01 09:53:47 +0800

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,37 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://ajelmarmedina.vercel.app'),
   title: {
     template: "%s | Ajel Medina",
-    default: "Ajelmar Medina?",
+    default: "Who's Ajelmar Medina?",
   },
   description: "Ajel Medina is a Front-end— Back-end freelancing developer and designer",
+  authors: {
+    name: "Ajelmar Medina",
+    url: "https://github.com/AjelmarMedina"
+  },
+  creator: "Ajelmar Medina",
+  keywords: ["ajel", "ajelmar", "ajelmar medina", "medina", "web developer", "front end", "back end", "web design"],
+  robots: "index, follow",
+  manifest: "./manifest.json",
+  openGraph: {
+    type: "website",
+    url: "https://ajelmarmedina.vercel.app",
+    title: "Ajelmar Medina",
+    description: "Ajelmar Medina's Portfolio website showcasing his Web Development career",
+    siteName: "Ajelmar Medina Portfolio",
+    images: [{
+      url: "/Heading.png",
+    }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@site",
+    creator: "@creator",
+    "images": "/Heading.png"
+
+  },
 };
 
 export default function RootLayout({
