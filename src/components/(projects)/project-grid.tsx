72bf4b3623f06e4e64a3cf5fade755b8b228e11e
AjelmarMedina <ajelmarmedina@gmail.com>
2024-04-24 21:01:39 +0800

    Selected project scrolling into view

--- src/components/(projects)/project-grid.tsx
+++ src/components/(projects)/project-grid.tsx
@@ -2,19 +2,29 @@
 
 import { projects } from "@/lib/data";
 import { cn } from "@/lib/utils";
-import { useCallback, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
+import { useState } from "react";
 import { Button } from "../ui/button";
+
+gsap.registerPlugin(ScrollToPlugin);
 
 export function ProjectGrid() {
   const [selected, setSelected] = useState<number | null>(null);
-  const logger = useCallback(() => {
-    console.log(selected);
-    
-  }, [selected])
-  
+  const { contextSafe } = useGSAP();
+
+  const scrollIntoView = contextSafe(() => {
+    gsap.to(window, {
+      scrollTo: "#project-grid",
+      duration: 0.7,
+    })
+  })
+
   return (
     <div
-        className={cn("flex flex-row justify-center items-center w-full h-fit text-neutral-black px-4 pb-14 md:px-28 md:pb-24", (selected && "md:h-screen"))}
+    id="project-grid"
+      className={cn("flex flex-row justify-center items-center w-full h-fit text-neutral-black px-4 pb-14 md:px-28 md:pb-24", (selected && "md:h-screen"))}
     >
       {selected === null && (
         <div className="w-full h-fit grid grid-rows-5 grid-cols-2 gap-4">
@@ -22,7 +32,10 @@ export function ProjectGrid() {
             <Button
               className="w-full aspect-[16/9] bg-project1 bg-accent-100"
               key={index}
-              onClick={() => setSelected(index)}
+              onClick={() => {
+                setSelected(index);
+                scrollIntoView();
+              }}
             >
               {project.title}
             </Button>
