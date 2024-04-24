8707b098799d9a1e44266fef0783e6ff74ee6ba4
AjelmarMedina <ajelmarmedina@gmail.com>
2024-04-24 22:39:32 +0800

    State change animation

--- src/components/(projects)/project-grid.tsx
+++ src/components/(projects)/project-grid.tsx
@@ -3,11 +3,11 @@
 import { projects } from "@/lib/data";
 import { cn } from "@/lib/utils";
 import { useGSAP } from "@gsap/react";
+import { AnimatePresence, motion } from "framer-motion";
 import gsap from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
 import { useState } from "react";
 import { Button } from "../ui/button";
-
 gsap.registerPlugin(ScrollToPlugin);
 
 export function ProjectGrid() {
@@ -17,43 +17,55 @@ export function ProjectGrid() {
   const scrollIntoView = contextSafe(() => {
     gsap.to(window, {
       scrollTo: "#project-grid",
-      duration: 0.7,
+      duration: 0.5,
     })
   })
 
   return (
     <div
-    id="project-grid"
-      className={cn("flex flex-row justify-center items-center w-full h-fit text-neutral-black px-4 pb-14 md:px-28 md:pb-24", (selected && "md:h-screen"))}
+      id="project-grid"
+      className={cn("flex flex-row justify-center items-center w-full text-neutral-black px-4 pb-14 md:px-28 md:pb-24", (selected && "md:h-screen"))}
     >
-      {selected === null && (
-        <div className="w-full h-fit grid grid-rows-5 grid-cols-2 gap-4">
-          {projects.map((project, index) => (
+      <AnimatePresence mode="popLayout">
+        {selected === null && (
+          <motion.div
+            key={"grid"}
+            className="w-full h-fit flex flex-col justify-center items-center space-y-4 sm:grid grid-rows-5 grid-cols-2 gap-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {projects.map((project, index) => (
+              <Button
+                className="w-full aspect-[16/9] bg-project1 bg-accent-100"
+                key={index}
+                onClick={() => {
+                  setSelected(index);
+                  scrollIntoView();
+                }}
+              >
+                {project.title}
+              </Button>
+            ))}
+          </motion.div>
+        )}
+        {selected !== null && (
+          <motion.div
+            key={"selected"}
+            className="flex flex-row justify-center items-center w-full lg:px-28"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
             <Button
               className="w-full aspect-[16/9] bg-project1 bg-accent-100"
-              key={index}
-              onClick={() => {
-                setSelected(index);
-                scrollIntoView();
-              }}
+              onClick={() => setSelected(null)}
             >
-              {project.title}
+              {projects[selected].title}
             </Button>
-          ))}
-        </div>
-      )}
-      {selected !== null && (
-        <div
-          className="flex flex-row justify-center items-center w-full lg:px-28"
-        >
-          <Button
-            className="w-full aspect-[16/9] bg-project1 bg-accent-100"
-            onClick={() => setSelected(null)}
-          >
-            {projects[selected].title}
-          </Button>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
