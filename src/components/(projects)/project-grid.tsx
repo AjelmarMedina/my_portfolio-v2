03c66e38f19127225d49543043a46a425fdb8fe0
AjelmarMedina <ajelmarmedina@gmail.com>
2024-04-25 12:48:59 +0800

    Badges

--- src/components/(projects)/project-grid.tsx
+++ src/components/(projects)/project-grid.tsx
@@ -6,7 +6,9 @@ import { useGSAP } from "@gsap/react";
 import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
 import gsap from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
+import { X } from "lucide-react";
 import { useState } from "react";
+import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 
 gsap.registerPlugin(ScrollToPlugin);
@@ -43,7 +45,7 @@ export function ProjectGrid() {
     return (
       <motion.div
         key={"grid"}
-        className="w-full h-fit flex flex-col justify-center items-center space-y-4 sm:grid grid-rows-5 grid-cols-2 gap-4"
+        className="w-full h-fit flex flex-col justify-center items-center space-y-4 sm:grid grid-rows-5 grid-cols-2 gap-4 sm:space-y-0"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
@@ -71,7 +73,7 @@ export function ProjectGrid() {
           }}
         >
           <motion.div
-            className="flex flex-col justify-center items-center w-full h-full overflow-hidden"
+            className="flex flex-col justify-center items-center space-y-2 w-full h-full overflow-hidden"
           >
             <LayoutGroup>
               <motion.h3 layout>
@@ -79,15 +81,19 @@ export function ProjectGrid() {
               </motion.h3>
               <AnimatePresence>
                 {isHovering && (
-                  <motion.p
-                    className="w-full text-center text-wrap"
+                  <motion.div
+                    layout
+                    className="flex flex-row flex-wrap space-x-1 justify-center items-center w-fit"
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
-                    layout
                   >
-                    {project.description}
-                  </motion.p>
+                    {project.tags.map((tag, index) => (
+                      <Badge key={index}>
+                        {tag}
+                      </Badge>
+                    ))}
+                  </motion.div>
                 )}
               </AnimatePresence>
             </LayoutGroup>
@@ -119,37 +125,57 @@ export function ProjectGrid() {
       const [isHovering, setHovering] = useState(false);
 
       return (
-        <Button
-          className="w-full aspect-[16/9] bg-project1 bg-accent-100"
+        <div
+          className="relative block w-full aspect-[16/9] bg-accent-100"
           onMouseOver={() => setHovering(true)}
           onMouseOut={() => setHovering(false)}
-          onClick={() => {
-            setSelected(null);
-          }}
         >
           <motion.div
-            className="flex flex-col justify-center items-center w-full h-full overflow-hidden"
+            className="flex flex-col justify-center items-start w-full h-full overflow-hidden px-6"
           >
-            <LayoutGroup>
-              <motion.h3 layout>
-                {project.title}
-              </motion.h3>
-              <AnimatePresence>
-                {isHovering && (
-                  <motion.p
-                    className="w-full text-center text-wrap"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    layout
-                  >
-                    {project.description}
-                  </motion.p>
-                )}
-              </AnimatePresence>
-            </LayoutGroup>
+            <div className="flex flex-col justify-center items-start space-y-4 w-[30%] h-full overflow-visible">
+              <LayoutGroup>
+                <motion.h3
+                  className="font-bold prose-display-xs"
+                  layout
+                >
+                  {project.title}
+                </motion.h3>
+                <motion.div
+                  layout
+                  className="flex flex-row flex-wrap space-x-1 justify-start items-center w-full"
+                >
+                  {project.tags.map((tag, index) => (
+                    <Badge key={index}>
+                      {tag}
+                    </Badge>
+                  ))}
+                </motion.div>
+                <AnimatePresence>
+                  {isHovering && (<>
+                    <motion.p
+                      className="w-full text-left text-wrap prose-text-lg"
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      layout
+                    >
+                      {project.description}
+                    </motion.p>
+                  </>)}
+                </AnimatePresence>
+              </LayoutGroup>
+            </div>
           </motion.div>
-        </Button>
+          <div
+            className="absolute p-2 right-0 top-0 z-10 w-fit h-fit cursor-pointer"
+            onClick={() => {
+              setSelected(null);
+            }}
+          >
+            <X width={32} height={32}/>
+          </div>
+        </div>
       )
     }
   }
